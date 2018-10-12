54faf905743b7ae3158a9c60910e09463bc08439
dina aid <dinaaid3@gmail.com>
2018-10-12 23:01:34 +0200

    version0.1

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// var closeX = document.getElementById('closeX');
+//  var openX = document.getElementById('openX');
+//  var sideMenu = document.getElementById ('sideMenu');
+
+//  function openNav() {
+//     sideMenu.style.width = "30%";
+// }
+
+//  function closeNav() {
+//     sideMenu.style.width = 0;
+// }
+
